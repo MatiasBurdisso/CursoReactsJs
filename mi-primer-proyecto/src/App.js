73752ba50c81dded54ac1a033fcf0e7798e77f78
MatiasB <matiasburdisso17@gmail.com>
2022-10-16 17:29:11 -0300

    retocando variables

--- mi-primer-proyecto/src/App.js
+++ mi-primer-proyecto/src/App.js
@@ -3,18 +3,21 @@ import './App.css';
 import {Navbar} from './componets/Navbar/Navbar';
 import {ItemListContainer} from './componets/itemlistcontainer/itemlistcontainer';
 import { Itemlist } from './componets/itemList/itemList';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-  
+  <BrowserRouter>
     <div className="App">
-      <header>
-        <Navbar/>
-      </header>
-      <ItemListContainer greeting = "lista" />
-      <Itemlist/>
+      <div>
+      <Navbar/>
+      <ItemListContainer />
+      <Routes>
+        
+      </Routes>
+      </div>
     </div>
-    
+  </BrowserRouter>
   );
 }
 
--- mi-primer-proyecto/src/componets/itemlistcontainer/itemlistcontainer.jsx
+++ mi-primer-proyecto/src/componets/itemlistcontainer/itemlistcontainer.jsx
@@ -1,9 +1,47 @@
-import {} from "./itemlistcontainer.css"
+import { arregloproductos } from "../itemList/item";
+import { Itemlist } from "../itemList/itemList";
+import { useParams } from "react-router-dom";
+import {useEffect, useState} from "react";
 
-export const ItemListContainer = ({greeting}) => {
+
+export const ItemListContainer = () => {
+    console.log(useParams());
+    const {categoryId} = useParams();
+    const [productos,Setproductos] = useState([]);
+
+    const promesa = new Promise((resolve, reject) => {
+        setTimeout(()=>{
+            resolve(arregloproductos);
+        },2000);
+    })
+
+    useEffect(()=>{
+        promesa.then((response)=>{
+            if(categoryId){
+                const productosFiltered = response.filter(elm=>elm.categoria === categoryId);
+                Setproductos(productosFiltered);
+            }else{
+                Setproductos(response)
+            }
+        })
+
+    },[categoryId])
+
+
+    /*const obtenerproductos = () =>{
+        return new Promise((resolve,reject)=>{
+            setTimeout(() => {
+                resolve(productos)
+            },5000);
+        })
+    }
+    useEffect(() =>{
+        obtenerproductos().them((result)=>console.log(result))
+    })*/
+    console.log("productos",productos);
     return(
         <div className="cuerpo">
-            {greeting}
+          <Itemlist items={productos}/>
         </div>
 
     )
